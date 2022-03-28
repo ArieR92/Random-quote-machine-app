3e624abfc2f1819c0c7b8f258dd2b6102ac4163d
Ariel <arielrodriguez0@outlook.com>
2022-03-27 18:35:02 -0600

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+
+
+const API  = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+//1- importar react
+//2- conseguir la API
+//3- crear el diseno
+//4- agregar los eventos
+//5- stilos
+//6- completar las historias de usuario
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <App/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
