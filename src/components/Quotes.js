ad0f9126ffc0348349475ea29e593a2bf7a625dc
Ariel <arielrodriguez0@outlook.com>
2022-03-27 20:35:25 -0600

    Second-commit

---
+++ src/components/Quotes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Quotes() {
+  return (
+    <div className='quote'>
+        <div className='anime' tittle='Hello'>Hello</div>
+
+        <blockquote>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eligendi inventore ex nam culpa assumenda alias amet a. Animi, quidem.</p>  
+        </blockquote>
+
+        character
+    </div>
+  )
+}
+
+export default Quotes
